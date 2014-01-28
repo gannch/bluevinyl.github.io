912fc16a486d17e80b899764e8482f4644c89c15
Blue Vinyl Creative <charlie@bluevinylcreative.com>
2014-01-28 16:49:48 -0600

    adjustsize
    javascript for responsive images

---
+++ adjustsize.js
@@ -0,0 +1,129 @@
+function adjustsize() {
+  $height = $(".section1").width() * 0.75;
+  $height = $height.toString();
+  $(".section1").css("height", $height + "px");
+  }
+
+$(document).ready(function() {
+  adjustsize();
+  $(window).resize(function() {
+    adjustsize();
+    });
+  });
+
+function adjustsize() {
+  $height = $(".section2").width() * 0.75;
+  $height = $height.toString();
+  $(".section2").css("height", $height + "px");
+  }
+
+$(document).ready(function() {
+  adjustsize();
+  $(window).resize(function() {
+    adjustsize();
+    });
+  });
+
+function adjustsize() {
+  $height = $(".section3").width() * 0.75;
+  $height = $height.toString();
+  $(".section3").css("height", $height + "px");
+  }
+
+$(document).ready(function() {
+  adjustsize();
+  $(window).resize(function() {
+    adjustsize();
+    });
+  });
+
+function adjustsize() {
+  $height = $(".section4").width() * 0.75;
+  $height = $height.toString();
+  $(".section4").css("height", $height + "px");
+  }
+
+$(document).ready(function() {
+  adjustsize();
+  $(window).resize(function() {
+    adjustsize();
+    });
+  });
+
+function adjustsize() {
+  $height = $(".section5").width() * 0.75;
+  $height = $height.toString();
+  $(".section5").css("height", $height + "px");
+  }
+
+$(document).ready(function() {
+  adjustsize();
+  $(window).resize(function() {
+    adjustsize();
+    });
+  });
+
+function adjustsize() {
+  $height = $(".section6").width() * 0.75;
+  $height = $height.toString();
+  $(".section6").css("height", $height + "px");
+  }
+
+$(document).ready(function() {
+  adjustsize();
+  $(window).resize(function() {
+    adjustsize();
+    });
+  });
+
+function adjustsize() {
+  $height = $(".section7").width() * 0.75;
+  $height = $height.toString();
+  $(".section7").css("height", $height + "px");
+  }
+
+$(document).ready(function() {
+  adjustsize();
+  $(window).resize(function() {
+    adjustsize();
+    });
+  });
+
+function adjustsize() {
+  $height = $(".section8").width() * 0.75;
+  $height = $height.toString();
+  $(".section8").css("height", $height + "px");
+  }
+
+$(document).ready(function() {
+  adjustsize();
+  $(window).resize(function() {
+    adjustsize();
+    });
+  });
+
+function adjustsize() {
+  $height = $(".section9").width() * 0.75;
+  $height = $height.toString();
+  $(".section9").css("height", $height + "px");
+  }
+
+$(document).ready(function() {
+  adjustsize();
+  $(window).resize(function() {
+    adjustsize();
+    });
+  });
+
+function adjustsize() {
+  $height = $(".section10").width() * 0.75;
+  $height = $height.toString();
+  $(".section10").css("height", $height + "px");
+  }
+
+$(document).ready(function() {
+  adjustsize();
+  $(window).resize(function() {
+    adjustsize();
+    });
+  });
